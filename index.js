8dead7828911de8d2bce2572beac175e70c7d954
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2026-03-25 23:47:50 +0600

    feat: enhance CORS configuration to dynamically allow all Vercel subdomains and include credentials.

--- index.js
+++ index.js
@@ -3,31 +3,46 @@ import dbConnector from "./config/dbConnector.js";
 import router from "./routes/allRouters.js";
 import path from "path";
 import { fileURLToPath } from "url";
-import cors from 'cors'
+import cors from "cors";
 
+const app = express();
 
-const app = express()
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://playvalult-client.vercel.app",
+];
 
-app.use(cors({
-    origin: ["http://localhost:5173" , "https://playvalult-client.vercel.app"],
-}))
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (
+      !origin ||
+      allowedOrigins.includes(origin) ||
+      origin.endsWith(".vercel.app")
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.json())
+app.use(express.json());
 
-
-app.get("/" , (req , res)=> {
-    res.send("The server is running")
-})
+app.get("/", (req, res) => {
+  res.send("The server is running");
+});
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+app.use("/api", router);
 
-app.use("/api", router)
-
-dbConnector()
-app.listen(3000, ()=> {
-    console.log("The server is running.")
-})
+dbConnector();
+app.listen(3000, () => {
+  console.log("The server is running.");
+});
